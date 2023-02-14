dbf29a66ddb6f58f839318c9e6afea718a261c21
Katariina83 <rikkonenkatariina@gmail.com>
2023-02-14 12:31:56 +0200

    first commit

---
+++ src/app/display-details/display-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-details',
+  templateUrl: './display-details.component.html',
+  styleUrls: ['./display-details.component.css']
+})
+export class DisplayDetailsComponent implements OnInit {
+  displayDetails = false;
+  log = [] as any;
+
+  onClick() {
+    this.displayDetails = !this.displayDetails;
+    this.log.push(this.log.length + 1);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
